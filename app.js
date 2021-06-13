87000ebbdc9e4226f1b7802ff558fc5b8adbedeb
V3ronez <v3ronez.dev@gmail.com>
2021-06-13 16:37:45 -0300

    feito a responsividade

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ function changeCircle(color) {
     const circle = document.querySelector('.circle')
     circle.style.background = color
 }
+
+function hambMenu(){
+    const menuToggle = document.querySelector('.toggle')
+    const navigation = document.querySelector('.navigation')
+
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
